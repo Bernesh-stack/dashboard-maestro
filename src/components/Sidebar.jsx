5734af454fcbb2dd3927a13cb33700fe611c1018
Bernesh-stack <bernesh.in@gmail.com>
2025-09-16 21:27:18 +0530

    NavBar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,21 +5,25 @@ import {MdOutlineCancel, MdOutlineDashboardCustomize,MdOutlinePersonPinCircle} f
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import {links} from "../data/dummy"
 import Button from './Button'
+import { useStateContext } from '../contexts/ContextProvider';  
 const Sidebar = () => {
-  const activeMenu = true;
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 ';
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 bg-blue-600';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu &&(
         <>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() =>{}} className='gap-3 ml-3 mt-4 flex items-center text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to="/" onClick={() =>setActiveMenu(false)} className='gap-3 ml-3 mt-4 flex items-center text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
               <SiShopware/> <span>Shopify</span>
           </Link>
           <TooltipComponent content="Menu" position='BottomCenter'>
-            <button type='button'  onClick={() =>{}} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
+            <button type='button'  onClick={() =>setActiveMenu(
+              (prevActiveMenu) => !prevActiveMenu
+            ) } className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
               <MdOutlineCancel/>
             </button>
 
@@ -33,14 +37,17 @@ const Sidebar = () => {
                 {item.title}
                 </p>
                 {item.links.map((link) =>(
-                  <NavLink to={`/${link.name}`}  key={link.name} onClick={({isActive})=>{}} className={(isActive)=>
-                    isActive ? activeLink : normalLink
-                  }>
+                  <NavLink 
+                    to={`/${link.name}`}  
+                    key={link.name} 
+                    onClick={() => {}} 
+                    className={({isActive}) =>
+                      isActive ? activeLink : normalLink
+                    }>
                     {link.icon}
                     <span className='capitalize'>
                       {link.name}
                     </span>
-
                   </NavLink>
                 ))}
               </div>
